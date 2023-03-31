51d7d80886a3949caa10b400f13d90a4632a5061
Seal5 <121348218+Seal5@users.noreply.github.com>
2023-03-31 00:44:45 -0400

    MongoDB Update

--- server/server.js
+++ server/server.js
@@ -10,6 +10,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(express.json());
+app.use(cors());
+
 app.use("/auth", userRouter);
 app.use("/meals", mealRouter); // Mount the mealRouter middleware
 
