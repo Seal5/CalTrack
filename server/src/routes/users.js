da90b81556f7c1204da2d6768757b8b2bcc49ab2
Seal5 <121348218+Seal5@users.noreply.github.com>
2023-07-26 16:29:44 -0400

    MongoDB Update

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { UserModel } from "../models/Users.js";
 
 const router = express.Router();
@@ -23,6 +24,28 @@ router.post("/register", async (req, res) => {
   await newUser.save();
 
   res.json({ message: "User registered successfully" });
+});
+
+router.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+    const user = await UserModel.findOne({ username }); 
+
+    if (!user) {
+        return res.json({ message: "Cannot find your account"})
+    } else {
+            const isPasswordValid = await bcrypt.compare(
+              password,
+              user.password 
+            );
+
+            if (isPasswordValid){
+                const token = jwt.sign({ id: user._id }, "secret")
+                return res.json({ token, userID: user._id })
+            } else {
+                return res.json({ message: "Wrong Password or Username" })
+            }
+    }
+
 });
 
 export { router as userRouter };
